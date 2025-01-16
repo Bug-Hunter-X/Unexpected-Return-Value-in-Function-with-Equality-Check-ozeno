ea3f5cbef6c0e77a5dbda3fb69ececae01750089
BugHunter-X <askzenai@gmail.com>
2025-01-16 23:24:04 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,10 @@
+function foo(a, b) {
+  if (a === b) {
+    return 0; // Or some other appropriate value
+  }
+  return a + b;
+}
+//Alternatively
+function foo(a,b){
+    return a===b ? 0 : a+b
+}
